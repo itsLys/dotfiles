fd04bd285cfd45237c972b5ae7d103210a0a0e6b
Lys <elyesseen@gmail.com>
2024-10-02 22:16:20 +0100

    added harpoon and rearranged

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -83,4 +83,92 @@ return {
 			end, { desc = "[S]earch [N]eovim files" })
 		end,
 	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+
+			-- REQUIRED
+			harpoon:setup()
+			-- REQUIRED
+
+			vim.keymap.set("n", "<leader>ha", function()
+				harpoon:list():add()
+			end, { desc = "[H]arpoon [A]dd buffer" })
+			vim.keymap.set("n", "<leader>hm", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end, { desc = "[H]arpoon Toggle [M]enu" })
+
+			vim.keymap.set("n", "<leader>h1", function()
+				harpoon:list():select(1)
+			end, { desc = "[H]arpoon Select [1]" })
+			vim.keymap.set("n", "<leader>h2", function()
+				harpoon:list():select(2)
+			end, { desc = "[H]arpoon Select [2]" })
+			vim.keymap.set("n", "<leader>h3", function()
+				harpoon:list():select(3)
+			end, { desc = "[H]arpoon Select [3]" })
+			vim.keymap.set("n", "<leader>h4", function()
+				harpoon:list():select(4)
+			end, { desc = "[H]arpoon Select [4]" })
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<leader>hn", function()
+				harpoon:list():next()
+			end, { desc = "[H]arpoon [N]ext" })
+			vim.keymap.set("n", "<leader>hp", function()
+				harpoon:list():prev()
+			end, { desc = "[H]arpoon [P]revious" })
+		end,
+	},
+	{
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup({
+				columns = { "icon" },
+				prompt_save_on_select_new_entry = false,
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-l>"] = false,
+					["<leader><C-s>"] = {
+						"actions.select",
+						opts = { vertical = true },
+						desc = "Open the entry in a vertical split",
+					},
+					["<leader><C-h>"] = {
+						"actions.select",
+						opts = { horizontal = true },
+						desc = "Open the entry in a horizontal split",
+					},
+					["<leader><C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+				},
+				view_options = {
+					show_hidden = true,
+				},
+			})
+			-- Open Explorer
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open Explorer" })
+			vim.keymap.set("n", "<leader>--", require("oil").toggle_float)
+		end,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 }
--- .config/workspace.sh
+++ .config/workspace.sh
@@ -1,1 +1,7 @@
 # TODO: tmux and nvim workspace script
+# 	start a tmux session 
+# 	splits it into windows and panes
+# 	opens certain files in certain windows
+# 	leaves a terminal for testing
+# 	takes args for files to open and number of window/panes to open
+# 	and puts files of certain types into 
