c4e7116b08fc6b6b56d2ecfeedeeee44a90bab36
Iliass (Lys) Hajji <elyesseen@gmail.com>
2024-11-20 14:49:39 +0100

    Update and rename colorscheme.lua to colors.lua

--- .config/nvim/lua/custom/colors.lua
+++ .config/nvim/lua/custom/colors.lua
@@ -1,28 +1,29 @@
 require("rose-pine").setup({
 	palette = {
-		main = {
-			base = "#00060A",
-			surface = "#000B12",
-			overlay = "#001524",
-			gold = "#EB6F92",
+		moon = {
+			base = "#181b25",
+			surface = "#1e222e",
+			overlay = "#282d3e",
+			gold = "#d1d4e1",
 		},
 	},
 	highlight_groups = {
-		Visual = { bg = "#011627", fg = "gold" },
+		-- Visual = { bg = "overlay", fg = "gold" },
 		WhichKeyBorder = { fg = "pine" },
 		WhichKeyGroup = { fg = "pine" },
 		WhichKey = { fg = "pine" },
-		String = { fg = "#3e8fb0" },
+		String = { fg = "#68aeca" },
 		["@keyword.import"] = { fg = "rose" },
 		["@type"] = { fg = "rose" },
 		["@string.escape"] = { fg = "iris" },
 	},
 })
 
-vim.cmd.colorscheme("rose-pine")
+vim.cmd.colorscheme("rose-pine-moon")
 vim.cmd.hi("@type gui=bold")
 vim.cmd.hi("Visual gui=bold")
 vim.cmd.hi("Comment gui=none")
 vim.cmd.hi("NonText guifg=bg")
 vim.cmd.hi("@parameter gui=bold")
 vim.cmd.hi("@keyword.import gui=bold")
+
